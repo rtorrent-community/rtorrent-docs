4e99a7b815d749c4fa0ffffc7428aa11c955d89d
pyroscope <pyroscope.project@gmail.com>
2017-08-27 14:50:25 +0200

    completion: add DD5.1 audio label

--- docs/examples/completion-path.sh
+++ docs/examples/completion-path.sh
@@ -52,9 +52,9 @@ pattern = re.compile(
     r"(?:[._ ](?P<release>UNRATED|REPACK|INTERNAL|PROPER|LIMITED|RERiP))*"
     r"(?:[._ ](?P<format>480p|576p|720p|1080p|1080i|2160p))?"
     r"(?:[._ ](?P<source>BDRip|BRRip|HDRip|DVDRip|DVD[59]?|PAL|NTSC|Web|WebRip|WEB-DL|Blu-ray|BluRay|BD25|BD50))"
-    r"(?:[._ ](?P<sound1>MP3|AC3|AAC(?:2.0)?|FLAC(?:2.0)?|DTS(?:-HD)?))?"
+    r"(?:[._ ](?P<sound1>MP3|DD5.1|AC3|AAC(?:2.0)?|FLAC(?:2.0)?|DTS(?:-HD)?))?"
     r"(?:[._ ](?P<codec>xvid|divx|avc|x264|h\.?264|hevc|h\.?265))"
-    r"(?:[._ ](?P<sound2>MP3|AC3|AAC(?:2.0)?|FLAC(?:2.0)?|DTS(?:-HD)?))?"
+    r"(?:[._ ](?P<sound2>MP3|DD5.1|AC3|AAC(?:2.0)?|FLAC(?:2.0)?|DTS(?:-HD)?))?"
     r"(?:[-.](?P<group>.+?))"
     r"(?P<extension>\.avi|\.mkv|\.m4v)?$", re.I
 )
